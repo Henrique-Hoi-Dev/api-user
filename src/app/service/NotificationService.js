066fbefb31ed38ef167220346805f189aa9d3ea3
Henrique Hoinacki <62766753+Henrique-Hoi-Dev@users.noreply.github.com>
2023-01-14 19:07:02 -0300

    mudança nas notificacao

--- src/app/service/NotificationService.js
+++ src/app/service/NotificationService.js
@@ -2,58 +2,78 @@ import httpStatus from 'http-status-codes';
 
 import User from '../models/User';
 // import Notification from "../schemas/Notification";
-import Notifications from "../models/Notification";
+import Notifications from '../models/Notification';
 
 export default {
   async getAll(req, res) {
-    let result = {}
+    let result = {};
 
     const checkIsMaster = await User.findOne({
-      where: { id: req.userId, type_role: "MASTER" }
-    })
+      where: { id: req.userId, type_role: 'MASTER' },
+    });
 
     if (!checkIsMaster) {
-      result = { httpStatus: httpStatus.BAD_REQUEST, msg: 'User not is Master' }      
-      return result
-    } 
-    
-    const notifications = await Notifications.findAll({
-      where: { user_id: req.userId, read: false },
-      order: [["created_at", "DESC"]],
-      attributes: [ 'id', 'content', 'read', 'created_at' ]
-    })
+      result = {
+        httpStatus: httpStatus.BAD_REQUEST,
+        msg: 'User not is Master',
+      };
+      return result;
+    }
 
-    result = { httpStatus: httpStatus.OK, status: "successful", dataResult: notifications } 
-    return result
+    const notifications = await Notifications.findAll({
+      where: { user_id: req.userId },
+      order: [['created_at', 'DESC']],
+      attributes: ['id', 'content', 'read', 'created_at'],
+    });
+
+    result = {
+      httpStatus: httpStatus.OK,
+      status: 'successful',
+      dataResult: notifications,
+    };
+    return result;
   },
 
-  async update(req, res) {   
-    let result = {}
+  async update(req, res) {
+    let result = {};
 
-    let notificationId = res.id
+    let notificationId = res.id;
 
-    const notificationReq = await Notifications.findByPk(notificationId)
+    const notificationReq = await Notifications.findByPk(notificationId);
 
     if (!notificationReq) {
-      result = { httpStatus: httpStatus.BAD_REQUEST, msg: 'Notification not found' }      
-      return result
+      result = {
+        httpStatus: httpStatus.BAD_REQUEST,
+        msg: 'Notification not found',
+      };
+      return result;
     }
 
     if (notificationReq.user_id === null) {
-      result = { httpStatus: httpStatus.BAD_REQUEST, msg: 'Do not have permission for this notification' }      
-      return result
+      result = {
+        httpStatus: httpStatus.BAD_REQUEST,
+        msg: 'Do not have permission for this notification',
+      };
+      return result;
     }
 
     if (notificationReq.read === true) {
-      result = {httpStatus: httpStatus.CONFLICT, dataResult: { msg: 'Has already been read.' }}      
-      return result
+      result = {
+        httpStatus: httpStatus.CONFLICT,
+        dataResult: { msg: 'Has already been read.' },
+      };
+      return result;
     }
 
-    await notificationReq.update({ read: true })
+    await notificationReq.update({ read: true });
 
-    const notification = await Notifications.findByPk(notificationId)
+    const notification = await Notifications.findByPk(notificationId);
 
-    result = { httpStatus: httpStatus.OK, status: "successful", dataResult: notification }      
-    return result
+    result = {
+      httpStatus: httpStatus.OK,
+      status: 'successful',
+      dataResult: notification,
+    };
+    return result;
   },
-}
+};
